e0d19dd6ea5c919d0daae9799c725ea327421bad
Félix Vicente <felixvicent1306@gmail.com>
2023-01-25 16:31:27 -0300

    chat container

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,3 +1,45 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../services/api";
+import { User } from "../../types/User";
+import * as S from "./styles";
+
 export function Chat() {
-  return <h1>Chat</h1>;
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState<User | undefined>(undefined);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("chatappuser")) {
+      navigate("/login");
+    } else {
+      const storagedUser = localStorage.getItem("chatappuser") || "";
+      setCurrentUser(JSON.parse(storagedUser));
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    async function fetchContacts() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const { data } = await api.get(`/api/users/${currentUser._id}`);
+
+          setContacts(data);
+
+          console.log(data);
+        } else {
+          navigate("set-avatar");
+        }
+      }
+    }
+
+    fetchContacts();
+  }, [currentUser, navigate]);
+
+  return (
+    <S.Container>
+      <div className="container"></div>
+    </S.Container>
+  );
 }
